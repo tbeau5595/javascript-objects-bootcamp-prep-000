f8b8b8120807adbd72b0b262a0b0cb7421ec3f83
Trey <tbeau5595@hotmail.com>
2018-09-11 01:38:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist ={ artist: "megadeath", song: "1320"};
 
-function updateplaylist(playlist, artistname, songTitle){
-  playlist.
+unction updatePlaylist(playlist, artist, song){
+  playlist[artist] = song;
+  return playlist;
 }
+ function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist
